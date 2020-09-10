1f86a194d242b89e484adb72495c169b11fa21cb
Koppisetti satya kumar <satyakoppisetti@gmail.com>
2020-09-10 15:02:50 -0400

    Created Page Model

--- database/migrations/2020_09_10_185238_create_pages_table.php
+++ database/migrations/2020_09_10_185238_create_pages_table.php
@@ -15,6 +15,11 @@ class CreatePagesTable extends Migration
     {
         Schema::create('pages', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->longText('content');
+            $table->string('meta_title');
+            $table->string('meta_description');
+            $table->tinyInteger('no_index')->default(1);
             $table->timestamps();
         });
     }
